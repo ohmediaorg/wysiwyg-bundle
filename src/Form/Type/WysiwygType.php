a2961650470f998cb44779749347fab4b2ac2157
Justin Hoffman <justin@ohmedia.ca>
2024-04-16 14:18:48 -0600

    wysywig type makes sure to set the class for JS

--- src/Form/Type/WysiwygType.php
+++ src/Form/Type/WysiwygType.php
@@ -9,10 +9,14 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormError;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class WysiwygType extends AbstractType
 {
+    public const WYSIWYG_HTML_CLASS = 'wysiwyg';
+
     private $wysiwyg;
 
     public function __construct(Wysiwyg $wysiwyg)
@@ -48,6 +52,25 @@ class WysiwygType extends AbstractType
                 }
             }
         );
+    }
+
+    public function buildView(FormView $view, FormInterface $form, array $options): void
+    {
+        if (!isset($view->vars['attr'])) {
+            $view->vars['attr'] = [];
+        }
+
+        if (!isset($view->vars['attr']['class'])) {
+            $view->vars['attr']['class'] = '';
+        }
+
+        $classes = explode(' ', $view->vars['attr']['class']);
+
+        if (!in_array(self::WYSIWYG_HTML_CLASS, $classes)) {
+            $classes[] = self::WYSIWYG_HTML_CLASS;
+        }
+
+        $view->vars['attr']['class'] = implode(' ', $classes);
     }
 
     public function getParent(): ?string
