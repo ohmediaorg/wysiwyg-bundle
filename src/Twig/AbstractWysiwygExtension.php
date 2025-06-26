6529c0fc80ca033e66a5082c08ee22502f697cc2
Justin Hoffman <justin@ohmedia.ca>
2025-06-26 15:17:23 -0600

    get rid of deprecation warnings

--- src/Twig/AbstractWysiwygExtension.php
+++ src/Twig/AbstractWysiwygExtension.php
@@ -6,27 +6,27 @@ use Twig\Extension\AbstractExtension;
 
 abstract class AbstractWysiwygExtension extends AbstractExtension
 {
-    final public function getTokenParsers()
+    final public function getTokenParsers(): array
     {
         return parent::getTokenParsers();
     }
 
-    final public function getNodeVisitors()
+    final public function getNodeVisitors(): array
     {
         return parent::getNodeVisitors();
     }
 
-    final public function getFilters()
+    final public function getFilters(): array
     {
         return parent::getFilters();
     }
 
-    final public function getTests()
+    final public function getTests(): array
     {
         return parent::getTests();
     }
 
-    final public function getOperators()
+    final public function getOperators(): array
     {
         return parent::getOperators();
     }
